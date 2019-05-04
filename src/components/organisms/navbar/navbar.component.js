1dc642c2ef8ab539112842027c6a1361e3e0cdf1
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-05-04 23:19:35 +0700

    feat(navbar): create layout navbar

--- src/components/organisms/navbar/navbar.component.js
+++ src/components/organisms/navbar/navbar.component.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
+import './style/style.scss';
+
 export default class NavbarComponent extends Component {
     render() {
         return (
-            <div className="navbar">
-                <div className="navbar__logo" />
-                <div className="footer__content">Navbar</div>
+            <div className="block navbar">
+                <div className="container flex">
+                    <div className="navbar__logo">
+                        <img
+                            src="https://public.urbanindo.com/images/urbanindo-2019.svg"
+                            alt="logo"
+                        />
+                    </div>
+                    <div className="navbar__content">Navbar</div>
+                </div>
             </div>
         );
     }
