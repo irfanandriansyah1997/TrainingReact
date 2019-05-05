44500a4c7baf9c1aa026d7600cabe2b5a852a4c8
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-05-05 19:49:57 +0700

    feat(service-worker): install workbox & clean webpack plugin

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
+/* eslint-disable */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { injectGlobal, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import Router from '@/routes';
 import Theme from '@/components/themes/default';
 
 import './styles/scss/app.scss';
-
-injectGlobal([
-    `
-body {
-    margin: 0;
-}
-`
-]);
 
 ReactDOM.render(
     <ThemeProvider theme={Theme}>
