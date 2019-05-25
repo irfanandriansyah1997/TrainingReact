ae1e29bc932ec71fb8fdeb303d97919419ea763e
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-05-26 00:06:17 +0700

    feat(component): register icon in storybook documentation

--- src/index.stories.js
+++ src/index.stories.js
@@ -7,9 +7,11 @@ import defaultTheme from '@/components/themes/default';
 const themes = [defaultTheme];
 
 import ButtonStorybook from '@/components/atoms/button/storybook/button.component.stories';
+import IconStorybook from  '@/components/atoms/icon/storybook/icon.component.stories';
 import TextStorybook from  '@/components/atoms/text/storybook/text.component.stories';
 
 storiesOf('Atomic Component', module)
     .addDecorator(withThemesProvider(themes))
     .add('Button', () => <ButtonStorybook />)
+    .add('Icon', () => <IconStorybook />)
     .add('Text', () => <TextStorybook />);
