c699db768deabbe001f9dabee44be7d0d9f222a3
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-05-26 00:05:57 +0700

    feat(component): create component icon

---
+++ src/components/atoms/icon/icon.component.js
@@ -0,0 +1,69 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+
+const getClassName = (children, size, type) => {
+    const response = {
+        'ui-atomic-icon': true,
+        'material-icons': type === 'material',
+        uif: type === 'urbanindo',
+        [children]: type === 'urbanindo'
+    };
+
+    if (typeof size === 'string') {
+        response[`ui-atomic-icon--${size}`] = true;
+    }
+
+    return Object.keys(response)
+        .filter(item => response[item])
+        .map(item => item)
+        .join(' ');
+};
+
+const getStyle = (color, size) => {
+    const response = {
+        color
+    };
+
+    if (typeof size === 'number') {
+        response.fontSize = `${size}px`;
+    }
+
+    console.log(response);
+
+    return response;
+};
+
+const IconComponent = ({
+    color,
+    children,
+    size,
+    type
+}) => ((
+    <i
+        className={getClassName(children, size, type)}
+        style={getStyle(color, size)}
+    >
+        { type === 'material' ? children : null}
+    </i>
+));
+
+IconComponent.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node, PropTypes.string
+    ]).isRequired,
+    color: PropTypes.string,
+    size: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.oneOf(['default', 'small'])
+    ]),
+    type: PropTypes.oneOf(['material', 'urbanindo'])
+};
+
+IconComponent.defaultProps = {
+    size: 'default',
+    color: '#3e4246',
+    type: 'material'
+};
+
+export default memo(IconComponent);
