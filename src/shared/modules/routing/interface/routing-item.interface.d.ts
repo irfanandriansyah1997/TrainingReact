525c94f788d8ae7f2554f17813ff7bf0d130ca4b
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-08-23 01:34:20 +0700

    feat(routing): setup routing url

---
+++ src/shared/modules/routing/interface/routing-item.interface.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Routing Item Interface
+ * @author Irfan Andriansyah <irfan@99.co>
+ * @since 2020.08.22
+ */
+export interface RoutingItemInterface {
+    path: string;
+    methodName: string;
+}
---
+++ src/shared/modules/routing/interface/routing-modules.interface.d.ts
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+/**
+ * Routing Modules Interface
+ * @author Irfan Andriansyah <irfan@99.co>
+ * @since 2020.08.23
+ */
+export interface RoutingModulesInterface {
+    render(): ReactNode;
+}
