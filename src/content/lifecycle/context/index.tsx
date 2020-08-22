ea457f0c5c4e90c1e563b653476775fcb8b162e1
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-08-23 01:35:54 +0700

    feat(router): setup router for lifecycle modules

---
+++ src/content/lifecycle/context/index.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from 'react';
+
+/**
+ * Context Modules
+ * @author Irfan Andriansyah <irfan@99.co>
+ * @since 2020.08.22
+ */
+const ContextModules: FunctionComponent = () => {
+    return (
+        <div className="heading">
+            Context Modules
+        </div>
+    );
+};
+
+export default ContextModules;
