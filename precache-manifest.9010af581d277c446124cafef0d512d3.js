f59b1b9d265beccaf269c07dca36de59b760d46f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-22 16:05:59 +0000

    Deploy irfanandriansyah1997/TrainingReact to github.com/irfanandriansyah1997/TrainingReact.git:gh-pages

---
+++ precache-manifest.9010af581d277c446124cafef0d512d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0399c3c73ef99fc000c75cf7c6c87392",
+    "url": "/training-react/index.html"
+  },
+  {
+    "revision": "14820cfbffa9995b68c8",
+    "url": "/training-react/static/js/2.de5ba474.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/training-react/static/js/2.de5ba474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea40c232b37aa25c106",
+    "url": "/training-react/static/js/main.c506d83f.chunk.js"
+  },
+  {
+    "revision": "8b1961629e38e130222e",
+    "url": "/training-react/static/js/runtime-main.dd6db1a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4438f6450a018ca85a8518aebc6e8f7b.js"
+  "/training-react/precache-manifest.9010af581d277c446124cafef0d512d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/training-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
